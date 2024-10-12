05d624e8c308da90f0c6a397ca51071a11106ab7
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-10-12 14:07:20 -0400

    terminado logout.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,10 +2,17 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { createAccessToken } from "../libs/jwt.js";
 
+//Todo: Para registrar el usuario.
 export const register = async (req, res) => {
-  const { email, password, username } = req.body;
-
   try {
+    const { email, password, username } = req.body;
+
+    const userFound = await User.findOne({ email });
+
+    if (userFound)
+      return res.status(400).json({
+        message: ["The email is already in use"],
+      });
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ email, password: hashedPassword, username });
 
@@ -25,10 +32,54 @@ export const register = async (req, res) => {
       // token,
     });
   } catch (error) {
-    console.error(error);
+    //Por si ocurre algún error.
+    res.status(500).json({ message: error.message });
   }
 };
+
+//todo: Para loguear el usuario.
 export const login = async (req, res) => {
-  const { email, password } = req.body;
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+    const userFound = await User.findOne({ email });
+
+    if (!userFound)
+      return res.status(400).json({
+        message: ["The email does not exist"],
+      });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: ["The password is incorrect"],
+      });
+    }
+
+    const token = await createAccessToken({
+      id: userFound._id,
+      username: userFound.username,
+    });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+//todo: Para borrar el token de la cookie.
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 };
